cd9955b370dc6fe1f2241b6aa261b8c45412e2d7
Józef Bongowski <j.bongowski@protonmail.com>
2019-08-09 21:36:53 +0200

    Add styling to the SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,18 @@ import React from "react";
 class SearchBar extends React.Component {
 	render() {
 		return (
-			<div>
-				<form>
-					<input type='text' />
-				</form>
+			<div className='ui segment center aligned'>
+				<div className='ui icon header'>
+					<i className='search icon' />
+					Search images
+				</div>
+				<div className='ui form'>
+					<div className='field'>
+						<div className='ui input'>
+							<input type='text' placeholder='Search...' />
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
