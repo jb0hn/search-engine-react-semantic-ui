5f381a22e5afcd4b5d1512df512c54f70a0afbff
Józef Bongowski <j.bongowski@protonmail.com>
2019-08-13 20:34:25 +0200

    Render images fetched from the API

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const ImageList = () => {
-	return <div>Image List</div>;
+const ImageList = props => {
+	return props.images.map(image => {
+		console.log(image.id);
+
+		return (
+			<li key={image.id}>
+				<img src={image.urls.regular} />
+			</li>
+		);
+	});
 };
 
 export default ImageList;
