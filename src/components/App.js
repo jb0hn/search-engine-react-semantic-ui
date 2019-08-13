4c5fe3792e55588e96c0ff8c7ed41f82214ec7ee
Józef Bongowski <j.bongowski@protonmail.com>
2019-08-13 14:34:18 +0200

    Connect to Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from "react";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
+import axios from "axios";
 
 import SearchBar from "./SearchBar";
 import ImageList from "./ImageList";
 
 class App extends React.Component {
 	onSearchSubmit(term) {
-		console.log(term);
+		axios.get("https://api.unsplash.com/search/photos", {
+			params: {
+				query: term
+			},
+			headers: {
+				Authorization:
+					"Client-ID e62006d4251dd7354db1d650496c1460c1e7ff331a44ed7993f32ac757a1331f"
+			}
+		});
 	}
 
 	render() {
