e1db39975fd830e9dfaacff2748a17222268d6ae
Józef Bongowski <j.bongowski@protonmail.com>
2019-08-13 15:51:03 +0200

    Finish basic network request setup

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,12 @@ import SearchBar from "./SearchBar";
 import ImageList from "./ImageList";
 
 class App extends React.Component {
-	onSearchSubmit(term) {
-		axios.get("https://api.unsplash.com/search/photos", {
+	state = {
+		images: []
+	};
+
+	onSearchSubmit = async term => {
+		const response = await axios.get("https://api.unsplash.com/search/photos", {
 			params: {
 				query: term
 			},
@@ -16,13 +20,17 @@ class App extends React.Component {
 					"Client-ID e62006d4251dd7354db1d650496c1460c1e7ff331a44ed7993f32ac757a1331f"
 			}
 		});
-	}
+		// console.log(this); debug: check if to what this property is assigned to
+
+		this.setState({ images: response.data.results });
+	};
 
 	render() {
 		return (
 			<div className='ui container' style={{ marginTop: "10px" }}>
 				<Helmet bodyAttributes={{ style: "background-color : #fff" }} />
 				<SearchBar onSubmit={this.onSearchSubmit} />
+				Found {this.state.images.length} images.
 				<ImageList />
 			</div>
 		);
