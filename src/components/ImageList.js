a32aaeab4fcd2441c0983d1a064522c6ad20b92a
Józef Bongowski <j.bongowski@protonmail.com>
2019-08-14 20:02:39 +0200

    Add basic grid to a list of an images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,13 @@
 import React from "react";
 
+import ImageCard from "./ImageCard";
+import "./ImageList.css";
+
 const ImageList = props => {
-	return props.images.map(({ id, urls, alt_description }) => {
-		return (
-			<div key={id}>
-				<img src={urls.regular} alt={alt_description} />
-			</div>
-		);
+	const images = props.images.map(image => {
+		return <ImageCard key={image.id} image={image} />;
 	});
+	return <div className='image-list'>{images}</div>;
 };
 
 export default ImageList;
